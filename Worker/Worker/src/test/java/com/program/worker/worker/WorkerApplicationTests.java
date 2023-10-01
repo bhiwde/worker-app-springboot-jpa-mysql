8270d713aa01233f1e75edfcdfc53e3c5b45b310
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-10-01 20:31:46 +0530

    [ADDED]:[Nirmla] Worker Inventory app Setup

---
+++ Worker/Worker/src/test/java/com/program/worker/worker/WorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.worker.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
